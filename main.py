44a9fc61cc2b58d8a8e0309020590ce251c64209
Yeferson Loaiza <45112889+loaiz@users.noreply.github.com>
2023-06-05 11:50:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import Depends, FastAPI, HTTPException, status,Form
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import UploadFile, File
+import os
+
+SECRET_KEY = "83daa0256a2289b0fb23693bf1f6034d44396675749244721a2b20e896e11662"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+#modulo usuarios
+db = {
+    "tim": {
+        "username": "tim",
+        "full_name": "Tim Ruscica",
+        "email": "tim@gmail.com",
+        "hashed_password": "$2b$12$HxWHkvMuL7WrZad6lcCfluNFj1/Zp63lvP5aUrKlSTYtoFzPXHOtu",
+        "disabled": False
+    }
+}
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str or None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+    hashed_password: str  # Added password field
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+upload_dir = os.path.join(os.path.dirname(__file__), "uploads")
+
+@app.post("/upload/image")
+async def upload_image(file: UploadFile = File(...), cedula: str = Form(...), nombre: str = Form(...)):
+    contents = await file.read()
+    with open(os.path.join(upload_dir, file.filename), "wb") as f:
+        f.write(contents)
+    return {"filename": file.filename, "cedula": cedula, "nombre": nombre}
